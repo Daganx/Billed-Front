4964acc4923aee89659155f5b270b321c05ca567
Dagan <114420715+Daganx@users.noreply.github.com>
2024-04-23 11:28:37 +0200

    ajout newBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,161 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen, waitFor} from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import BillsUI from "../views/BillsUI.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES_PATH,ROUTES } from "../constants/routes.js";
+import router from "../app/Router.js";
 
+jest.mock("../app/Store.js");
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then the form should appairs", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then the form should appear", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const form = document.querySelector('form[data-testid="form-new-bill"]');
+      expect(form).toBeTruthy();
+    });
+
+    test("Then email icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const windowIcon = screen.getByTestId("icon-mail");
+      //to-do write expect expression
+      expect(windowIcon.classList.contains("active-icon")).toBeTruthy();
+    });
+
+    describe("handleChangeFile", () => {
+      test("Then alert should display when uploading a file with an invalid extension", () => {
+        const localStorageMock = {
+          getItem: jest
+            .fn()
+            .mockReturnValue(JSON.stringify({ email: "test@example.com" })),
+        };
+        document.body.innerHTML = `
+          <form data-testid="form-new-bill">
+            <input type="file" data-testid="file" />
+          </form>
+        `;
+        
+        const alertSpy = jest
+          .spyOn(window, "alert")
+          .mockImplementation(() => {});
+
+        const newBill = new NewBill({
+          document,
+          onNavigate: jest.fn(),
+          store: null,
+          localStorage: localStorageMock,
+        });
+       
+        const fileInput = document.querySelector('input[data-testid="file"]');
+        const file = new File(["dummy content"], "test.txt", {
+          type: "text/plain",
+        });
+        Object.defineProperty(fileInput, "files", {
+          value: [file],
+        });
+        // Trigger file change event
+        fileInput.dispatchEvent(new Event("change"));
+        // Expect an alert to be displayed
+        expect(alertSpy).toHaveBeenCalledWith(
+          "Veuillez sélectionner un fichier avec une extension jpg, jpeg ou png."
+        );
+        expect(fileInput.value).toBe("");
+        // Restore the original implementation of window.alert
+        alertSpy.mockRestore();
+      });
+    });
+    test("Should contain all required inputs", () => {
+      document.body.innerHTML = NewBillUI();
+
+      const expenseTypeInput = document.querySelector(
+        'select[data-testid="expense-type"]'
+      );
+      const expenseNameInput = document.querySelector(
+        'input[data-testid="expense-name"]'
+      );
+      const dateInput = document.querySelector(
+        'input[data-testid="datepicker"]'
+      );
+      const amountInput = document.querySelector('input[data-testid="amount"]');
+      const vatInput = document.querySelector('input[data-testid="vat"]');
+      const pctInput = document.querySelector('input[data-testid="pct"]');
+      const commentaryInput = document.querySelector(
+        'textarea[data-testid="commentary"]'
+      );
+      const fileInput = document.querySelector('input[data-testid="file"]');
+      const submitButton = document.querySelector('button[type="submit"]');
+
+      // Assert that all required inputs exist
+      expect(expenseTypeInput).toBeTruthy();
+      expect(expenseNameInput).toBeTruthy();
+      expect(dateInput).toBeTruthy();
+      expect(amountInput).toBeTruthy();
+      expect(vatInput).toBeTruthy();
+      expect(pctInput).toBeTruthy();
+      expect(commentaryInput).toBeTruthy();
+      expect(fileInput).toBeTruthy();
+      expect(submitButton).toBeTruthy();
+    });
+
+    test("Should submit the form with correct data", () => {
+      document.body.innerHTML = NewBillUI();
+
+      const handleSubmitSpy = jest.spyOn(NewBill.prototype, "handleSubmit");
+
+      const newBillInstance = new NewBill({
+        document: document,
+        onNavigate: jest.fn(),
+        store: null,
+        localStorage: {
+          getItem: jest.fn(() => JSON.stringify({ email: "test@example.com" })),
+        },
+      });
+
+      // Simulate form submission
+      const form = document.querySelector('form[data-testid="form-new-bill"]');
+      form.dispatchEvent(new Event("submit"));
+
+      // Assert that handleSubmit is called
+      expect(handleSubmitSpy).toHaveBeenCalled();
+
+      // Restore the original method after the test
+      handleSubmitSpy.mockRestore();
+    });
+    test('Then it fails with a 404 message error', async () => {
+      const errorMessage = 'Erreur 404';
+      const html = BillsUI({ error: errorMessage });
+      document.body.innerHTML = html;
+
+      const message = await screen.getByText(errorMessage);
+      expect(message).toBeTruthy();
+    });
+
+    test('Then it fails with a 500 message error', async () => {
+      const errorMessage = 'Erreur 500';
+      const html = BillsUI({ error: errorMessage });
+      document.body.innerHTML = html;
+
+      const message = await screen.getByText(errorMessage);
+      expect(message).toBeTruthy();
+    });    
+  });
+});
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -8,6 +8,9 @@ export default class NewBill {
     this.onNavigate = onNavigate
     this.store = store
     const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+    // Liaison des méthodes aux instances pour garantir que les méthodes ont le bon contexte
+    this.handleChangeFile = this.handleChangeFile.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     formNewBill.addEventListener("submit", this.handleSubmit)
     const file = this.document.querySelector(`input[data-testid="file"]`)
     file.addEventListener("change", this.handleChangeFile)
@@ -55,27 +58,27 @@ export default class NewBill {
         })
         .catch(error => console.error(error));
 }
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    const email = JSON.parse(localStorage.getItem("user")).email
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending'
-    }
-    
-    this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
-  }
+handleSubmit(e) {
+  e.preventDefault();
+  console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value);
+  const email = JSON.parse(localStorage.getItem("user")).email;
+  const bill = {
+    email,
+    type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+    name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+    amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+    date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+    vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+    pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+    commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+    fileUrl: this.fileUrl,
+    fileName: this.fileName,
+    status: 'pending'
+  };
+  
+  this.updateBill(bill);
+  this.onNavigate(ROUTES_PATH['Bills']);
+}
 
   // not need to cover this function by tests
   updateBill = (bill) => {
